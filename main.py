a17f0d979cdd7110384d662688bce4af4d93c861
조성주 <joseongju@joseongjuui-MacBookAir.local>
2023-08-22 14:34:17 +0900

    update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pytesseract
 import io
 import uvicorn
 import re
+from typing import List
 
 app = FastAPI()
 
@@ -11,62 +12,80 @@ app = FastAPI()
 def read_root():
     return {"message": "이미지 인식 api"}
 
-@app.post("/text")
-async def upload_image(file: UploadFile = File(...)):
-    try:
-        contents = await file.read()
-        image = Image.open(io.BytesIO(contents))
-        extracted_text = pytesseract.image_to_string(image, lang='kor')
+def clean_product_name(product_name: str) -> str:
+    # 제품명에서 "교환처", "유효기간", "주문번호"와 같은 불필요한 문자열 제거
+    remove_patterns = ["교환처", "유효기간", "주문번호", r"\d{4,8}\s*\d{4,8}", r"\d{4}\.\d{1,2}\.\d{1,2}"]
+    for pattern in remove_patterns:
+        product_name = re.sub(pattern, "", product_name)
+    # 여러 공백 및 줄바꿈을 한 공백으로 치환
+    product_name = re.sub(r"\s+", " ", product_name).strip()
+    return product_name
 
-        return {"extracted_text": extracted_text}
+async def extract_text_from_image(file: UploadFile):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    extracted_text = pytesseract.image_to_string(image, lang='kor')
+    return extracted_text
 
-    except pytesseract.TesseractError as te:
-        raise HTTPException(status_code=400, detail=f"Tesseract OCR Error: {te}")
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"General Error: {e}")
+def extract_info_from_text(extracted_text: str) -> dict:
+    if "쿠폰상태" in extracted_text:
+        status_match = re.search(r"쿠폰상태\s+([\w]+)", extracted_text)
+        status = status_match.group(1).strip() if status_match else "null"
+        return {"coupon_status": status}
+    else:
+        # 제품명 추출
+        product_name_match = re.search(r"(.*?)(?=\n\n\d{4,8}\s*\d{4,8}|\n\n\d{4,8})", extracted_text, re.DOTALL)
+        product_name = product_name_match.group(1).strip() if product_name_match else "null"
+        product_name = clean_product_name(product_name)
+    
+        # 교환처 추출
+        exchange_match = re.search(r"교환처\s*([^\n]+)", extracted_text)
+        exchange_place = exchange_match.group(1).strip() if exchange_match else "null"
+
+        # 유효기간 추출
+        expiration_match = re.search(r"유효기간\s*([^\n]+)", extracted_text)
+        if not expiration_match:
+            expiration_match = re.search(r"(\d{4}[.년]\s*\d{1,2}[.월]*\s*\d{1,2}[일]*)", extracted_text)
+        expiration_date = expiration_match.group(1).strip() if expiration_match else "null"
+
+
+    return {
+        "product_name": product_name,
+        "exchange_place": exchange_place,
+        "expiration_date": expiration_date
+    }
 
 @app.post("/upload")
-async def upload_image(file: UploadFile = File(...)):
-    try:
-        contents = await file.read()
-        image = Image.open(io.BytesIO(contents))
-        extracted_text = pytesseract.image_to_string(image, lang='kor')
+async def upload_images(files: List[UploadFile] = File(...)):
+    results = []
 
-        expiration_match = re.search(r"유효기간\s+([\d년\s월일]+)", extracted_text)
-        status_match = re.search(r"쿠폰상태\s+([\w]+)", extracted_text)
-        product_name_match = re.search(r"<\s*.*\s*선물하기.*\s*\n\n(.*?)(?=\n\n\d+)", extracted_text, re.DOTALL)
-        exchange_place_match = re.search(r"교환처\s*([^\n]+)", extracted_text)
+    for file in files:
+        try:
+            extracted_text = await extract_text_from_image(file)
+            info = extract_info_from_text(extracted_text)
+            results.append(info)
 
-        if expiration_match:
-            expiration_date = expiration_match.group(1).replace("\n", " ").strip()
-        else:
-            expiration_date = "null"
+        except pytesseract.TesseractError as te:
+            raise HTTPException(status_code=400, detail=f"Tesseract OCR Error: {te}")
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"General Error: {e}")
 
-        if status_match:
-            status = status_match.group(1).replace("\n", " ").strip()
-        else:
-            status = "null"
+    return results
 
-        if product_name_match:
-            product_name = product_name_match.group(1).replace("\n", " ").strip()
-            #product_name = " ".join([line for line in product_name.split() if not line.isdigit()])
-        else:
-            product_name = "null"
+@app.post("/text")
+async def upload_image(files: List[UploadFile] = File(...)):
+    results = []
 
-        if exchange_place_match:
-            exchange_place = exchange_place_match.group(1).replace("\n", " ").strip()
-        else:
-            exchange_place = "null"
+    for file in files:
+        try:
+            contents = await file.read()
+            image = Image.open(io.BytesIO(contents))
+            extracted_text = pytesseract.image_to_string(image, lang='kor')
 
-        return {
-            "expiration_date": expiration_date,
-            "coupon_status": status,
-            "product_name": product_name,
-            "exchange_place" : exchange_place
-        }
+            return {"extracted_text": extracted_text}
 
-    except pytesseract.TesseractError as te:
-        raise HTTPException(status_code=400, detail=f"Tesseract OCR Error: {te}")
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"General Error: {e}")
-
+        except pytesseract.TesseractError as te:
+            raise HTTPException(status_code=400, detail=f"Tesseract OCR Error: {te}")
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"General Error: {e}")
+            
