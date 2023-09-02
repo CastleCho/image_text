48ff41e3328bb8bcceb8047dc3d42aee02ce85fd
조성주 <joseongju@joseongjuui-MacBookAir.local>
2023-09-02 12:09:41 +0900

    update

--- main.py
+++ main.py
@@ -74,6 +74,8 @@ def extract_info_from_text(extracted_text: str) -> dict:
         product_name = product_name_match.group(1).strip()
         
     product_name = clean_product_name(product_name)
+    if product_name == '64':
+        product_name = 'CU'
     info['product_name'] = product_name
 
     exchange_match = re.search(r"교환처\s*([^\n]+)", extracted_text)
